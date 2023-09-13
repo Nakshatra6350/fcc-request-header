3170d1c4e3cd348d0c7adcadbd0ab28597cb23cd
Replit user <>
2023-09-13 04:47:22 +0000

    all

--- index.js
+++ index.js
@@ -20,8 +20,14 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const userLanguage = req.headers['accept-language']
+  // const response = {
+  //   language:userLanguage
+  // }
+  res.json({ ipaddress: req.ip,
+           language: userLanguage,
+           software: req.headers['user-agent']});
 });
 
 // listen for requests :)
